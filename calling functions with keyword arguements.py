be01f8588bb49106ebbe089f90a5ae83f019faa3
KelvinMugo4500 <87333463+KelvinMugo4500@users.noreply.github.com>
2022-03-09 16:17:12 +0300

    Add files via upload

---
+++ calling functions with keyword arguements.py
@@ -0,0 +1,8 @@
+def student(firstname,lastname ='Mark',standard='Fifth'):
+    print(firstname,lastname,'studies in',standard,'Standard')
+#1 keyword arguement
+student(firstname = 'John')
+#2 keyword arguements
+student(firstname = 'John',standard = 'Seventh')
+#2 keyword arguements
+student(lastname = 'Gates',firstname = 'John')
