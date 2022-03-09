45449edadae41949b1ccac5f2b37a6058ac87bc6
KelvinMugo4500 <87333463+KelvinMugo4500@users.noreply.github.com>
2022-03-09 16:17:36 +0300

    Add files via upload

---
+++ calling functions without keyword arguements.py
@@ -0,0 +1,9 @@
+def student(firstname,lastname ='Mark',standard='Fifth'):
+    print(firstname,lastname,'studies in',standard,'Standard')
+#1 positional arguement
+student('John')
+#3 positional arguements
+student('John','Gates','Seventh')
+#2 positional arguements
+student('John','Gates')
+student('John','Seventh')
